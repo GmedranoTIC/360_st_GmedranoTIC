0db1dabe49dfe6bdf48a33bf5573956f20f4b94a
Guillermo Medrano <gmedranotic@gmail.com>
2026-02-21 21:39:48 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -60,11 +60,12 @@ async function addLogoToNadir(imageSrc: string): Promise<string> {
         
         // Crear logo SVG
         //const svgData = `<svg width="${logoSize}" height="${logoSize}" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-	const svgData = `<svg width="${logoSize}" height="${logoSize}*.15" viewBox="0 0 7000 400" xmlns="http://www.w3.org/2000/svg">
+	const svgData = `<svg width="${logoSize}" height="${logoSize}*.15" viewBox="0 0 7000 350" xmlns="http://www.w3.org/2000/svg">
 
   //<circle cx="24" cy="24" r="22" fill="#92400e" stroke="#fff" stroke-width="2"/>
-<rect x="24" y="24" width="7000" height="400" fill="#92400e" stroke="#fff" stroke-width="2"/>
-  <text x="3500" y="250" font-size="200" font-weight="bold" fill="white" text-anchor="middle" font-family="sans-serif">3 6 0   G m e d r a n o T I C   s t u d i o   -   2 0 2 6 </text>
+//buena <rect x="24" y="24" width="7000" height="400" fill="#92400e" stroke="#fff" stroke-width="2"/>
+<rect x="24" y="24" width="img.width" height="350" fill="#92400e" stroke="#fff" stroke-width="2"/>
+  <text x="3500" y="250" font-size="200" font-weight="bold" fill="white" text-anchor="middle" font-family="sans-serif">360 GmedranoTIC studio - 2026 </text>
   <text x="24" y="36" font-size="6" fill="rgba(255,255,255,0.8)" text-anchor="middle" font-family="sans-serif">Studio</text>
 </svg>`;
         
