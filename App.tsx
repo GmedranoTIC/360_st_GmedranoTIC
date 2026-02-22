acbfd93082cdc6cef814b01f376a9bf9913bd953
Guillermo Medrano <gmedranotic@gmail.com>
2026-02-22 19:43:25 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -67,7 +67,7 @@ async function addLogoToNadir(imageSrc: string): Promise<string> {
 	//buena <rect x="24" y="24" width="7000" height="400" fill="#92400e" stroke="#fff" stroke-width="2"/>
 	<rect x="0" y="-10" width="${logoSize}" height="50" fill="#ffff00" />
 	<rect x="0" y="0" width="${logoSize}" height="500" fill="#92400e" stroke="#fff" stroke-width="0"/>
-	<rect x="0" y="400" width="${logoSize}" height="200" fill="#fff0000" />
+	<rect x="0" y="400" width="${logoSize}" height="200" fill="#ff0000" />
  	<text x="3535" y="120" font-size="130" fill="white" text-anchor="middle" font-family="sans-serif" letter-spacing="155">360 GmedranoTIC studio - 2026 - </text>
   	//<text x="24" y="36" font-size="6" fill="rgba(255,255,255,0.8)" text-anchor="middle" font-family="sans-serif"></text>
 </svg>`;
