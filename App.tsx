d5dcc8ab5aaaa69d986711c2bb610b93e7bcd900
Guillermo Medrano <gmedranotic@gmail.com>
2026-02-22 19:04:50 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -55,7 +55,7 @@ async function addLogoToNadir(imageSrc: string): Promise<string> {
         const logoSize = Math.max(img.width, img.height); // 100% del tamaño mayor
         const centerX = img.width / 2;
         //const nadirY = img.height * 0.85; // 85% hacia abajo
-        const nadirY = img.height * 0.92; // 92% hacia abajo
+        const nadirY = img.height * 0.93; // 93% hacia abajo
         
         console.log('  [addLogoToNadir] Logo size:', logoSize, 'at position:', centerX, nadirY);
         
@@ -65,7 +65,7 @@ async function addLogoToNadir(imageSrc: string): Promise<string> {
 
   	//<circle cx="24" cy="24" r="22" fill="#92400e" stroke="#fff" stroke-width="2"/>
 	//buena <rect x="24" y="24" width="7000" height="400" fill="#92400e" stroke="#fff" stroke-width="2"/>
-	<rect x="0" y="0" width="7050" height="350" fill="#92400e" stroke="#fff" stroke-width="0"/>
+	<rect x="0" y="0" width="7050" height="400" fill="#92400e" stroke="#fff" stroke-width="0"/>
  	<text x="3500" y="170" font-size="130" fill="white" text-anchor="middle" font-family="sans-serif" letter-spacing="145">360 GmedranoTIC studio - 2026 - </text>
   	//<text x="24" y="36" font-size="6" fill="rgba(255,255,255,0.8)" text-anchor="middle" font-family="sans-serif"></text>
 </svg>`;
